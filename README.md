7138caa0e09a23607159ff1b900f972edf6ef36f
Roger Gabiru <roger.gabiru@gmail.com>
2023-07-26 10:23:07 -0300

    readme.md

---
+++ README.md
@@ -0,0 +1,36 @@
+# Personal Portfolio
+
+My personal portfolio, made from scratch, contains several elements that I learned to improve, in a harmonious and well-articulated way. However, this is still the first version and some other features will be added:
+
+- [x] Responsiveness using minimal media query
+- [ ] Light/Dark theme switching
+- [ ] Portuguese/English language selection
+
+_____
+
+## Table of contents
+
+- [Overview](#overview)
+	- [Screenshot](#screenshot)
+	- [Links](#links)
+	- [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+### Screenshot
+
+![](./screenshot_desktop.jpeg) 
+![](./screenshot_mobile.jpeg)
+
+### Links
+- Live Site URL: [Github Pages](https://gabirueitz.github.io/portfolio/)
+
+### Built with
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Grid
+- JavaScript
+
+## Contact Me:
+[<img width="48" height="48" src="https://img.icons8.com/color/48/github--v1.png" alt="github--v1"/>](https://github.com/gabirueitz) [<img width="48" height="48" src="https://img.icons8.com/color/48/linkedin.png" alt="linkedin"/>](https://www.linkedin.com/in/roger-gabiru-goncalves/) [<img width="48" height="48" src="https://img.icons8.com/fluency/48/instagram-new.png" alt="instagram-new"/>](https://www.instagram.com/gabirueitz/) [<img width="48" height="48" src="https://img.icons8.com/color/48/twitter--v1.png" alt="twitter--v1"/>](https://www.twitter.com/gabirueitz) [<img width="48" height="48" src="https://img.icons8.com/color/48/discord-logo.png" alt="discord-logo"/>](https://discord.com/users/697486281596207126)
--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,33 @@
+/* --------------- Toggle Themes --------------- */
+let initialTheme = true;
+
+function toggleThemes() {
+    const root = document.documentElement;
+
+    if (initialTheme) {
+        root.style.setProperty('--color1', '#');
+        root.style.setProperty('--color2', '#');
+        root.style.setProperty('--color3', '#');
+        root.style.setProperty('--color4', '#');
+        root.style.setProperty('--color5', '#');
+        initialTheme = false;
+    } else {
+        root.style.setProperty('--color1', '#03091A');
+        root.style.setProperty('--color2', '#1F1F32');
+        root.style.setProperty('--color3', '#CACFD6');
+        root.style.setProperty('--color4', '#fff950');
+        root.style.setProperty('--color5', '#35acac');
+        initialTheme = true;
+    }
+}
+
+/* ------------- Paralax Effects ------------- */
 function reveal() {
     var reveals = document.querySelectorAll(".toAnimate");
     for (var i = 0; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
         var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
+        var elementVisible = 100;
         if (elementTop < windowHeight - elementVisible) {
             reveals[i].classList.add("active");
         } else {
@@ -14,7 +38,7 @@ function reveal() {
 
 window.addEventListener("scroll", reveal);
 
-/* --------------- Menu --------------- */
+/* ------------------ Menu ------------------ */
 const burguer = document.getElementById('burguer');
 const header = document.getElementById('header');
 
